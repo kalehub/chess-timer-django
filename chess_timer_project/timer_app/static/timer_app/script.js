eeb0a13e584f09f7eeb76cff5c06eeec78e106ad
kalehub <=>
2021-04-14 17:57:34 +0800

    fixing set timer button on index, fixing form in set-timer page, adding JS in set-timer clock options UI

--- chess_timer_project/timer_app/static/timer_app/script.js
+++ chess_timer_project/timer_app/static/timer_app/script.js
@@ -1,8 +1,30 @@
+/**
+ * @file             : script.js
+ * @author           : Teguh Satya <teguhsatyadhr@gmail.com>
+ * Date              : 14.04.2021
+ * Last Modified Date: 14.04.2021
+ * Last Modified By  : Teguh Satya <teguhsatyadhr@gmail.com>
+ */
+
+let hours = "00";
+let minutes = "00";
+let seconds = "00";
+
+
+
 function nextForm(){
     console.log('form button clicked!');
     prevForm = document.querySelector('#firstForm');
     nextForm = document.querySelector('#secondForm');
     prevForm.style.display='none';
     nextForm.classList.remove('d-none');
+
+}
+
+function clockClicked(val){
+    gameClock = hours+':'+val+':'+seconds;
+    document.querySelector('#gameClock').innerHTML = gameClock;
+    
+    
 
 }
